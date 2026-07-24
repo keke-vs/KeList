69d64abfc7a0d92f242813ea958d74ddb7b45d22
keke-vs <3312446293@qq.com>
2026-07-24 10:55:43 +0800

    Add keList source

---
+++ src/keList/Models/AppSettings.cs
@@ -0,0 +1,15 @@
+namespace KeList.Models;
+
+public sealed class AppSettings
+{
+    public double Left { get; set; } = double.NaN;
+    public double Top { get; set; } = double.NaN;
+    public double Width { get; set; } = 390;
+    public double Height { get; set; } = 520;
+    public double FontSize { get; set; } = 16;
+    public double BackgroundOpacity { get; set; } = 0.82;
+    public bool IsTopmost { get; set; } = true;
+    public bool IsLocked { get; set; }
+    public bool StartWithWindows { get; set; }
+    public bool HasShownTrayHint { get; set; }
+}
